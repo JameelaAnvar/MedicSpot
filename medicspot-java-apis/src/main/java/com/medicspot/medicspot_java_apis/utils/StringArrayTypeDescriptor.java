258819a88ba8b59b0807dc4f6f920286527da710
I344755 <himanshu.ranjan@sap.com>
2018-08-11 16:39:06 +0530

    Files to support array store in postgres

---
+++ medicspot-java-apis/src/main/java/com/medicspot/medicspot_java_apis/utils/StringArrayTypeDescriptor.java
@@ -0,0 +1,16 @@
+/*package com.medicspot.medicspot_java_apis.utils;
+
+public class StringArrayTypeDescriptor extends AbstractArrayTypeDescriptor<String[]> {
+
+	public static final StringArrayTypeDescriptor INSTANCE = new StringArrayTypeDescriptor();
+
+	public StringArrayTypeDescriptor() {
+		super(String[].class);
+	}
+
+	@Override
+	protected String getSqlArrayType() {
+		return "text";
+	}
+}
+*/
